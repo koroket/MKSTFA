d91417df12fbada01588328cafc720ffa33ea7e2
luke_solomon@me.com <lukesolomon@Lukes-MacBook-Pro.local>
2014-09-26 13:39:39 -0700

    initial commit
    This is the first commit, adding the Xcode project

---
+++ TFE/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TFE
+//
+//  Created by Luke Solomon on 9/26/14.
+//  Copyright (c) 2014 SoloBando Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
